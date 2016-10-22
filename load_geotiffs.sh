2660c9966c441beab797bcbf06dfebf11ae0feb4
Joe Farned <joefarned@gmail.com>
2016-10-22 14:30:31 -0400

    geotiffs successfully loaded in postgis

--- load_geotiffs.sh
+++ load_geotiffs.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-IMAGEPATH=$1
+DBNAME=$1
+TABLENAME=$2
+IMAGEPATH=$3
 
 mkdir -p $IMAGEPATH/sql
 
+# Could this be parallelized?
 for f in $IMAGEPATH/*.tif
 do
-    raster2pgsql -I -s 4236 $f > "$IMAGEPATH/sql/$(basename $f)".sql
+    raster2pgsql -a -s 4236 $f public.$TABLENAME > "$IMAGEPATH/sql/$(basename $f)".sql
 done
 
+psql -d $DBNAME -c "CREATE TABLE "public"."$TABLENAME" ("rid" serial PRIMARY KEY,"rast" raster);"
+psql -d $DBNAME -c "SELECT AddRasterConstraints('public','eightbands','rast',TRUE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE,TRUE,TRUE,TRUE);"
+
+# Could this be parallelized?
 for f in $IMAGEPATH/sql/*.sql
 do
-    psql -d spacenet -f $f
+    psql -d $DBNAME -f $f
 done
