4d1f0e828a61bffe8b5f6829cd9aa04e556cd4dd
Joe Farned <joefarned@gmail.com>
2016-10-25 15:58:27 -0400

    wrong srid (oops)

--- load_geotiffs.sh
+++ load_geotiffs.sh
@@ -6,16 +6,16 @@ IMAGEPATH=$3
 
 mkdir -p $IMAGEPATH/sql
 
-# Could this be parallelized?
+# TODO: parallelize
 for f in $IMAGEPATH/*.tif
 do
-    raster2pgsql -a -s 4236 $f public.$TABLENAME > "$IMAGEPATH/sql/$(basename $f)".sql
+    raster2pgsql -a -s 4326 $f public.$TABLENAME > "$IMAGEPATH/sql/$(basename $f)".sql
 done
 
 psql -d $DBNAME -c "CREATE TABLE "public"."$TABLENAME" ("rid" serial PRIMARY KEY,"rast" raster);"
 psql -d $DBNAME -c "SELECT AddRasterConstraints('public','eightbands','rast',TRUE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE,TRUE,TRUE,TRUE);"
 
-# Could this be parallelized?
+# TODO: parallelize
 for f in $IMAGEPATH/sql/*.sql
 do
     psql -d $DBNAME -f $f
